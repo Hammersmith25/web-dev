aad3a008dd26e5c8c099ee24275ed879cbd02471
Danil G <danil.goncharov25@gmail.com>
2026-02-17 00:38:28 +0500

    Define a Route

--- lab4/tutorial/src/app/user/user.ts
+++ lab4/tutorial/src/app/user/user.ts
@@ -1,30 +1,9 @@
 import {Component} from '@angular/core';
-import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-user',
-  template: `
-    <p>Username: {{ username }}</p>
-    <p>Preferred Framework:</p>
-    <ul>
-      <li>
-        Static Image:
-        <img ngSrc="/logo.svg" alt="Angular logo" />
-      </li>
-      <li>
-        Dynamic Image:
-        <img [ngSrc]="logoUrl" [alt]="logoAlt" />
-      </li>
-    </ul>
-    <div class="image-container">
-      <img ngSrc="www.example.com/image.png" fill />
-      <img ngSrc="www.example.com/image.png" height="600" width="800" priority />
-    </div>
-  `,
-  imports: [NgOptimizedImage],
+  template: ` <div>Username: {{ username }}</div> `,
 })
 export class User {
-  logoUrl = '/logo.svg';
-  logoAlt = 'Angular logo';
   username = 'youngTech';
 }
