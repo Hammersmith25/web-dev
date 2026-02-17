f855d3f343d8256da4dfed16ac3c3cded55c6cd8
Danil G <danil.goncharov25@gmail.com>
2026-02-17 12:26:17 +0500

    Reactive Forms

--- lab4/tutorial/src/app/app.ts
+++ lab4/tutorial/src/app/app.ts
@@ -1,16 +1,36 @@
 import {Component} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
+import { ReactiveFormsModule , FormControl, FormGroup  } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   template: `
-    <nav>
-      <a routerLink="/">Home</a>
-      |
-      <a routerLink="/user">User</a>
-    </nav>
-    <router-outlet />
+    <form [formGroup]="profileForm" (ngSubmit)="handleSubmit()">
+      <label>
+        Name
+        <input type="text" formControlName="name" />
+      </label>
+      <label>
+        Email
+        <input type="email" formControlName="email" />
+      </label>
+      <button type="submit">Submit</button>
+      <h2>Profile Form</h2>
+      <p>Name: {{ profileForm.value.name }}</p>
+      <p>Email: {{ profileForm.value.email }}</p>
+    </form>
   `,
-  imports: [RouterOutlet,RouterLink],
+  imports: [ ReactiveFormsModule],
 })
-export class App {}
+export class App {
+  
+  profileForm = new FormGroup({
+  name: new FormControl(''),
+  email: new FormControl(''),
+  });
+
+  handleSubmit() {
+    alert(
+      this.profileForm.value.name + ' | ' + this.profileForm.value.email
+    );
+  }
+}
