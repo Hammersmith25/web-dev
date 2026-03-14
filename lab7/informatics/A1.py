75c7c42a2c0dcbbdfe0ae27cd516992468ce7e84
Danil G <danil.goncharov25@gmail.com>
2026-03-14 12:56:59 +0500

    start

---
+++ lab7/informatics/A1.py
@@ -0,0 +1,4 @@
+import math
+a = float(input())
+b = float(input())
+print(math.sqrt(pow(a,2) + pow(b,2)))
---
+++ lab7/informatics/B1.py
@@ -0,0 +1,4 @@
+a = int(input("ENTER:"))
+next = a + 1
+prev = a - 1
+print(f"next: {next}, prev: {prev}")
